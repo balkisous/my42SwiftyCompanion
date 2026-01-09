5fee68cebc5a857699ca36b48e89ed1989331964
balkisous <balkisous@gmail.com>
2026-01-09 18:26:05 +0100

    Initial Commit

---
+++ my42SwiftyCompanion/my42SwiftyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  my42SwiftyCompanionApp.swift
+//  my42SwiftyCompanion
+//
+//  Created by balkis wang on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct my42SwiftyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
